ebf83ca92033ebc8e02523b6a9dec7ecc478fc49
aryan <aryan.sharma@vwo.com>
2024-11-11 17:35:35 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import "../styles/globals.css";
+import { VWOScript } from "inject-smartcode-next";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <VWOScript 
+        accountId="955578"
+        type="ASYNC"
+        settingsTimeout={2000}
+        hideElement="body"
+        hideElementStyle="opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;transition:none !important;"
+        scriptAttributes={{
+          strategy: "beforeInteractive"
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { VWOScript } from "npm-next-smartcode";
 
 export default function Document() {
   return (
@@ -8,17 +7,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <VWOScript 
-          accountId="955578"
-          type="ASYNC"
-          settingsTimeout={2000}
-          hideElement="body"
-          hideElementStyle="opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;transition:none !important;"
-          scriptAttributes={{
-            id: "vwoCode",
-            strategy: "beforeInteractive"
-          }}
-        />
       </body>
     </Html>
   );
